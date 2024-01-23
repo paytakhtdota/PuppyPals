c3146dff01830ab471728b3185f89e779adf3484
Mo Ansari <paytakhtdota@gmail.com>
2024-01-22 17:14:59 -0800

    App.jsx is functional

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,13 +1,38 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
